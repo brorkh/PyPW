5d8ade16ae6ce361d646151641db712dcf304286
Tom <brorkh@users.noreply.github.com>
2022-09-12 13:54:23 +0200

    Add files via upload

---
+++ tpw.py
@@ -0,0 +1,8 @@
+import hashlib
+t1 = input("Bitte was eingeben: ")
+# Salt eingeben
+s1 = "#SALT"
+# encode it to bytes using UTF-8 encoding
+message = t1.encode()
+# hash with MD5 (not recommended)
+print("PW: ", hashlib.sha256(message).hexdigest()[0:18],s1,sep='')
