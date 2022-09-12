27686ce0c0cd0e0e5240c909d2401a5232558e95
Tom <brorkh@users.noreply.github.com>
2022-09-12 13:50:19 +0200

    Delete tpw.py

--- tpw.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-t1 = input("Bitte was eingeben: ")
-s1 = "#T"
-# encode it to bytes using UTF-8 encoding
-message = t1.encode()
-# hash with MD5 (not recommended)
-print("WLAN-PW: ", hashlib.sha256(message).hexdigest()[0:18],s1,sep='')
