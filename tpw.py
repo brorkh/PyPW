25d5a8f3170705ce3efb3520132de974977b06cc
Tom <brorkh@users.noreply.github.com>
2022-09-13 10:56:27 +0200

    Update tpw.py

--- tpw.py
+++ tpw.py
@@ -1,7 +1,7 @@
 import hashlib
 t1 = input("Bitte was eingeben: ")
 # Salt eingeben
-s1 = "#SALT"
+s1 = input("Salt eingeben: ")
 # encode it to bytes using UTF-8 encoding
 message = t1.encode()
 # hash with MD5 (not recommended)
